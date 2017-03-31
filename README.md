59d19d17ec2657da0e8c3976cd6a6394b0ef0aed
Catherine Bacon <catherine@Catherines-MacBook.local>
2017-03-31 10:48:28 +0100

    add about page content and update README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # pinterest clone
+
+This app was created as part of the following FreeCodeCamp challenge: https://www.freecodecamp.com/challenges/build-a-pinterest-clone
+
+**Final app: UPDATE**
+
+## Objective
+
+Build a full stack JavaScript app that is functionally similar to [this](https://midnight-dust.hyperdev.space)
+
+### User Stories:
+
+- As an unauthenticated user, I can login with Twitter. *As I don't have a twitter account I've chose to login with Google*.
+- As an authenticated user, I can link to images.
+- As an authenticated user, I can delete images that I've linked to.
+- As an authenticated user, I can see a Pinterest-style wall of all the images I've linked to.
+- As an unauthenticated user, I can browse other users' walls of images.
+- As an authenticated user, if I upload an image that is broken, it will be replaced by a placeholder image.
+
+## Libraries/Frameworks
+
+I used [Meteor](https://www.meteor.com/) as the platform. Combined with [React](https://guide.meteor.com/react.html).
+
+I also used [React Router](https://github.com/ReactTraining/react-router/tree/master/packages/react-router) for the routing.
+
+It is styled with [react-bootstrap](https://react-bootstrap.github.io/) and the Journal theme from [bootswatch](https://bootswatch.com/).
+
+I also used [react-masonry-component](https://github.com/eiriklv/react-masonry-component) to display the image thumbnails and [react-visibility-sensor](https://github.com/joshwnj/react-visibility-sensor) to allow infinite scrolling.
--- imports/ui/pages/About.jsx
+++ imports/ui/pages/About.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Meteor } from 'meteor/meteor';
 import { Row, Col, PageHeader } from 'react-bootstrap';
 
 export default class About extends Component {
@@ -10,7 +9,102 @@ export default class About extends Component {
           <PageHeader>About</PageHeader>
         </Col>
         <Col>
-          <p>content</p>
+          <p>
+            This app was created as part of the following FreeCodeCamp challenge:
+            {' '}
+            <a
+              href="https://www.freecodecamp.com/challenges/build-a-pinterest-clone"
+            >
+              https://www.freecodecamp.com/challenges/build-a-pinterest-clone
+            </a>
+          </p>
+          <p>
+            <strong>
+              Final app:
+              {' '}
+              <a href="#">
+                UPDATE
+              </a>
+            </strong>
+          </p>
+        </Col>
+        <Col>
+          <h3>Objective</h3>
+          <p>
+            Build a full stack JavaScript app that is functionally similar to
+            {' '}
+            <a href="https://midnight-dust.hyperdev.space">this</a>
+            .
+          </p>
+          <h5>User Stories:</h5>
+          <ul>
+            <li>
+              As an unauthenticated user, I can login with Twitter.
+              {' '}
+              <em>
+                As I don't have a twitter account I've chose to login with Google
+              </em>.
+            </li>
+            <li>As an authenticated user, I can link to images.</li>
+            <li>
+              As an authenticated user, I can delete images that I've linked to.
+            </li>
+            <li>
+              As an authenticated user, I can see a Pinterest-style wall of all the images I've linked to.
+            </li>
+            <li>
+              As an unauthenticated user, I can browse other users' walls of images.
+            </li>
+            <li>
+              As an authenticated user, if I upload an image that is broken, it will be replaced by a placeholder image.
+            </li>
+          </ul>
+        </Col>
+        <Col>
+          <h3>Libraries/Frameworks</h3>
+          <p>
+            I used
+            {' '}
+            <a href="https://www.meteor.com/">Meteor</a>
+            {' '}
+            as the platform. Combined with
+            {' '}
+            <a href="https://guide.meteor.com/react.html">React</a>
+            . I also used
+            {' '}
+            <a
+              href="https://github.com/ReactTraining/react-router/tree/master/packages/react-router"
+            >
+              React Router
+            </a>
+            {' '}
+            for the routing.
+          </p>
+          <p>
+            It is styled with
+            {' '}
+            <a href="https://react-bootstrap.github.io/">react-bootstrap</a>
+            {' '}
+            and the Journal theme from
+            {' '}
+            <a href="https://bootswatch.com/">boostswatch</a>
+            .
+          </p>
+          <p>
+            I also used
+            {' '}
+            <a href="https://github.com/eiriklv/react-masonry-component">
+              react-masonry-component
+            </a>
+            {' '}
+            to display the image thumbnails and
+            {' '}
+            <a href="https://github.com/joshwnj/react-visibility-sensor">
+              react-visibility-sensor
+            </a>
+            {' '}
+            to allow infinite scrolling.
+          </p>
         </Col>
       </Row>
     );
